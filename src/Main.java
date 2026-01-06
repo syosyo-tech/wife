7b4477de546eb3dd4aa562fc3f14e09266727145
syosyo-tech <sh_tsukahara@icloud.com>
2026-01-06 21:20:40 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("----------プログラムを開始します----------");
+        System.out.println("----------プログラムを終了します----------");
+    }
+}
